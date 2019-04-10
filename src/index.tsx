df711dfb6cf933d8850c5c94c84133b1eb357fad
Sam Slotsky <sam@manifold.co>
2019-04-10 09:25:34 -0500

    Attempt to add embedded marketplace

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { Component } from "react";
 import { render } from "react-dom";
+import { defineCustomElements } from "@manifoldco/ui/esm/es2017/manifold.define";
+
+defineCustomElements(window);
+
 // import './index.css'
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return <manifold-marketplace />;
   }
 }
 
